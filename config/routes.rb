4bc85410492ebb687d7c6feda2e3b085f8e998b7
thanhhung270502 <thanhhung270502@gmail.com>
2022-08-16 09:32:45 +0700

    Create Static_Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
