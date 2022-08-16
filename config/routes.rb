c0b25cd6bb086a2add7eb8c7d69bfed90fb05693
thanhhung270502 <thanhhung270502@gmail.com>
2022-08-16 16:29:56 +0700

    Update UI/Controller Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: "main#index"
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/contact'
+
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
