886e2bf788617c96ec4c0fccae11c6d6861ed5e2
thanhhung270502 <thanhhung270502@gmail.com>
2022-08-28 12:25:52 +0700

    Add javascript, button Top

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
-
-console.log("Hello world!")
+import "./custom/menu"
+import "./custom/top"
+console.log("ABC");
---
+++ app/javascript/custom/top.js
@@ -0,0 +1,19 @@
+//Get the button
+console.log("Hello World!");
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.onclick = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
